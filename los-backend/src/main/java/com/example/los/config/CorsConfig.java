7341fa6de1cf3784247dd7700b2cf07e30983705
ktknn <kientri0310@gmail.com>
2026-09-18 20:21:17 +0700

    feat: fix corsconfig

--- los-backend/src/main/java/com/example/los/config/CorsConfig.java
+++ los-backend/src/main/java/com/example/los/config/CorsConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origins}")
+    @Value("${FRONTEND_URL}")
     private String allowedOrigins;
 
     @Override
